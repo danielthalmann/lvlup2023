941e9e5a5563febfe8d9e46c68cfd4eeb1f140d7
leandro camilo <leandrocamilo@leandros-MacBook-Pro.local>
2023-02-18 20:23:45 +0100

    Pushed for daniel

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,13 +10,11 @@ public class Movement : MonoBehaviour
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        if (Input.GetButtonDown("Jump") && IsGrounded() && name == "Bon")
+        if (Input.GetButtonDown("Jump") && name == "Bon")
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
@@ -26,11 +24,6 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-    }
-
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
     private void Flip()
