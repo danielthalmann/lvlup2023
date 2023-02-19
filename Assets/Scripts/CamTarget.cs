a414bd9d696c04bd06922d3595d0976568228273
leandro camilo <leandrocamilo@leandros-MacBook-Pro.local>
2023-02-19 10:54:41 +0100

    temp

--- Assets/Scripts/CamTarget.cs
+++ Assets/Scripts/CamTarget.cs
@@ -23,4 +23,21 @@ public class CamTarget : MonoBehaviour
         Vector3 SmoothPosition = Vector3.Lerp(cam.transform.position, desiredPosition, smoothSpeed);
         cam.transform.position = SmoothPosition;
     }
+
+    [SerializeField] private Rigidbody2D rb;
+    public GameObject bullet;
+    public Transform throwPoint;
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.tag == "Truand" && Input.GetKeyDown(KeyCode.E))
+            Shooting();
+    }
+
+    void Shooting()
+    {
+        GameObject bulletClone = (GameObject)Instantiate(bullet, throwPoint.position, throwPoint.rotation);
+        bulletClone.transform.localScale = transform.localScale;
+        Destroy(bulletClone, 0.5f);
+    }
 }
