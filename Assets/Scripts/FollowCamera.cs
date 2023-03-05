e6a1ada0076002b6a84c960916d4b8744cb6c25b
Daniel Thalmann <daniel@thalmann.li>
2023-03-05 23:23:39 +0100

    update script name

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class FollowCam : MonoBehaviour
+public class FollowCamera : MonoBehaviour
 {
 
     public GameObject cam;
